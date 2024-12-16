9cb91b4151d4c5db33cccb2ef93c99469fdf44e0
Dayanand <105712823+Dayananda-D@users.noreply.github.com>
2024-12-16 11:28:12 +0530

    commit 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
-import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Animated, FlatList, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import Swiper from 'react-native-swiper';
+import SVGComponent from './svgComponent';
 
-const AnimatedBtn=Animatable. createAnimatableComponent (TouchableOpacity)
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity)
 const HomeScreen = () => {
   const swiperRef = useRef(null); // Create a ref to control Swiper
 
@@ -18,13 +20,26 @@ const HomeScreen = () => {
     }
   };
 
+  const headerHieght = useSharedValue(0);
+
+  const headerStyle = useAnimatedStyle(() => {
+    return {
+      height: headerHieght.value,
+    };
+  });
+
+  useEffect(() => {
+    headerHieght.value = withTiming(350, { duration: 1500 });
+  }, []);
+
+
   return (
     <Swiper
       ref={swiperRef}
       loop={false}
       scrollEnabled={false}
       showsPagination={false} // Hide indicators
-      
+
     >
       {/* Welcome Screen */}
       <ImageBackground
@@ -33,7 +48,7 @@ const HomeScreen = () => {
       >
         <View style={styles.container}>
           <Animatable.Text style={styles.title} animation={'slideInUp'}>Welcome to FitLife!</Animatable.Text>
-          <Animatable.Text style={{ width:'80%',color:'#9e9e9e',fontSize:16,marginTop: 50,fontWeight:'600'}} animation={'slideInUp'}>Elevate Your Fitness with a Cutting Edge to Fuel Your Motivation & Crush your goal</Animatable.Text>
+          <Animatable.Text style={{ color: '#9e9e9e', textAlign: 'center', fontSize: 16, paddingTop: 100, fontWeight: '600' }}>Elevate Your Fitness with a Cutting Edge to Fuel Your Motivation & Crush your goal</Animatable.Text>
           <AnimatedBtn style={styles.startButton} onPress={goToNextSlide} animation={'slideInUp'}>
             <Text style={styles.buttonText}>Get Started!</Text>
           </AnimatedBtn>
@@ -41,82 +56,131 @@ const HomeScreen = () => {
       </ImageBackground>
 
       {/* Gender Selection Screen */}
-      <View style={styles.genderContainer}>
-        <Text style={styles.title}>What's your gender?</Text>
-        <View style={styles.genderImagesContainer}>
-          {/* Male Image */}
-          <TouchableOpacity
-            style={styles.genderImageWrapper}
-            onPress={goToNextSlide}
-          >
-            <Image
-              source={require('./../../assets/images/male.jpg')}
-              style={styles.genderImage}
-            />
-            <Text style={styles.genderText}>Male</Text>
-          </TouchableOpacity>
+      <ImageBackground
+        source={require('./../../assets/images/bg.png')}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.genderContainer}>
+          <Text style={styles.title}>What's your gender?</Text>
+          <View style={styles.genderImagesContainer}>
+            {/* Male Image */}
+            <TouchableOpacity
+              style={styles.genderImageWrapper}
+              onPress={goToNextSlide}
+            >
+              <Image
+                source={require('./../../assets/images/male.jpg')}
+                style={styles.genderImage}
+              />
+              <Text style={styles.genderText}>Male</Text>
+            </TouchableOpacity>
 
-          {/* Female Image */}
-          <TouchableOpacity
-            style={styles.genderImageWrapper}
-            onPress={goToNextSlide}
-          >
-            <Image
-              source={require('./../../assets/images/female.jpg')}
-              style={styles.genderImage}
-            />
-            <Text style={styles.genderText}>Female</Text>
+            {/* Female Image */}
+            <TouchableOpacity
+              style={styles.genderImageWrapper}
+              onPress={goToNextSlide}
+            >
+              <Image
+                source={require('./../../assets/images/female.jpg')}
+                style={styles.genderImage}
+              />
+              <Text style={styles.genderText}>Female</Text>
+            </TouchableOpacity>
+          </View>
+          {/* Next Screen (Example) */}
+          <TouchableOpacity style={styles.backButton} onPress={goToPrevSlide}>
+            <Text style={styles.backButtonText}>Back</Text>
           </TouchableOpacity>
         </View>
-        {/* Next Screen (Example) */}
-        <TouchableOpacity style={styles.backButton} onPress={goToPrevSlide}>
-          <Text style={styles.backButtonText}>Back</Text>
-        </TouchableOpacity>
-      </View>
+      </ImageBackground>
 
       {/* Goal Selection Screen */}
       <ImageBackground
-        source={require('./../../assets/images/goal.jpeg')}
+        source={require('./../../assets/images/bg.png')}
         style={styles.backgroundImage}
       >
-      <View style={styles.slide}>
-        <Text style={styles.title}>What’s Your Goal?</Text>
-        <TouchableOpacity style={styles.goalButton} onPress={goToNextSlide}>
-          <Image source={require('./../../assets/images/weight-loss.png')} style={styles.goalImage}/>
-          <Text style={styles.buttonText}>Lose Weight</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.goalButton} onPress={goToNextSlide}>
-          <Image source={require('./../../assets/images/muscular.jpg')} style={styles.goalImage}/>
-          <Text style={styles.buttonText}>Build Muscle</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.goalButton} onPress={goToNextSlide}>
-          <Image source={require('./../../assets/images/fit.jpg')} style={styles.goalImage}/>
-          <Text style={styles.buttonText}>Keep Fit</Text>
-        </TouchableOpacity>
-        {/* Next Screen (Example) */}
-        <TouchableOpacity style={styles.backButton} onPress={goToPrevSlide}>
-          <Text style={styles.backButtonText}>Back</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.container}>
+          <Text style={styles.title}>What’s Your Goal?</Text>
+          <TouchableOpacity style={styles.goalButton} onPress={goToNextSlide}>
+            <Image source={require('./../../assets/images/weight-loss.png')} style={styles.goalImage} />
+            <Text style={styles.buttonText}>Lose Weight</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.goalButton} onPress={goToNextSlide}>
+            <Image source={require('./../../assets/images/muscular.jpg')} style={styles.goalImage} />
+            <Text style={styles.buttonText}>Build Muscle</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.goalButton} onPress={goToNextSlide}>
+            <Image source={require('./../../assets/images/fit.jpg')} style={styles.goalImage} />
+            <Text style={styles.buttonText}>Keep Fit</Text>
+          </TouchableOpacity>
+          {/* Next Screen (Example) */}
+          <TouchableOpacity style={styles.backButton} onPress={goToPrevSlide}>
+            <Text style={styles.backButtonText}>Back</Text>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
 
-      {/* Body Parts Selection Screen */}
-      <View style={styles.slide}>
-        <Text style={styles.title}>Select Body Parts for Workout</Text>
-        <TouchableOpacity style={styles.button} onPress={goToNextSlide}>
-          <Text style={styles.buttonText}>Arms</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={goToNextSlide}>
-          <Text style={styles.buttonText}>Legs</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={goToNextSlide}>
-          <Text style={styles.buttonText}>Core</Text>
-        </TouchableOpacity>
-        {/* Next Screen (Example) */}
-        <TouchableOpacity style={styles.backButton} onPress={goToPrevSlide}>
-          <Text style={styles.backButtonText}>Back</Text>
-        </TouchableOpacity>
-      </View>
+      {/* Injury Body Parts Selection Screen */}
+      <ImageBackground source={require('./../../assets/images/bg.png')} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <Text style={styles.title}>What do you want to workout ?</Text>
+          <SVGComponent width={'60%'} height={'70%'} />
+          <TouchableOpacity style={styles.backButton} onPress={goToPrevSlide}>
+            <Text style={styles.backButtonText}>Back</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.startButton} onPress={goToNextSlide}>
+            <Text style={styles.buttonText}>Let's Begin</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+
+
+      {/* DashBoard*/}
+      <ImageBackground
+        source={require('./../../assets/images/bg.png')}
+        style={styles.backgroundImage}
+      >
+        <Animated.View style={[{ width: '100%', height: 350, backgroundColor: '#43527a', borderBottomLeftRadius: 30, borderBottomRightRadius: 30 }, headerStyle]}>
+        </Animated.View>
+        <View style={{ width: '100%', height: '100%', position: 'absolute' }}>
+          <View style={{ width: '100%', alignContent: 'center', marginTop: 70, justifyContent: 'space-around', flexDirection: 'row', alignItems: 'center' }}>
+            <TouchableOpacity onPress={goToPrevSlide}>
+              <Image source={require('../../assets/images/menu.png')} style={{ width: 24, height: 24, tintColor: 'white' }} />
+            </TouchableOpacity>
+            <Text style={{ color: 'white', fontSize: 20, fontWeight: '500' }}> Fit Buddy</Text>
+            <Image source={require('../../assets/images/male.jpg')} style={{ width: 50, height: 50, borderRadius: 25 }} />
+          </View>
+          <Text style={{ color: 'white', fontSize: 18, alignSelf: 'center', marginTop: 49 }}>Here is your warm ups</Text>
+          {/* <Text style={{ color: 'white', fontSize: 30, alignSelf: 'center', marginTop: 10 }}>Hii</Text> */}
+          <View style={{ width: '100%', height: 100, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around', }}>
+            <TouchableOpacity style={{ backgroundColor: '#000', padding: 10, borderRadius: 10 }}>
+              <Text style={styles.backButtonText}>Arms</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.backButtonText}>Chest</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.backButtonText}>Abdominal</Text>
+            </TouchableOpacity>
+            <TouchableOpacity >
+              <Text style={styles.backButtonText}>Biceps</Text>
+            </TouchableOpacity>
+            <TouchableOpacity >
+              <Text style={styles.backButtonText}>legs</Text>
+            </TouchableOpacity>
+          </View>
+          <FlatList data={[1, 1, 1, 1, 1, 1, 1, 1, 1]} renderItem={({ item }) => {
+            return (
+              <Animated.View style={{ width: '90%', height: 100, marginTop: 20, backgroundColor: '#f2f2f2', alignSelf: 'center', borderRadius: 20 }}>
+                <View style={{ width: '90%', marginTop: 20, justifyContent: 'space-between', alignSelf: 'center', flexDirection: 'row' }}>
+                  <Image source={require('../../assets/images/treadmill.jpg')} style={{ width: 50, height: 50 }} />
+                  <Text style={{ color: '#9e9e9e' }}>Start Now</Text>
+                </View>
+              </Animated.View>
+            )
+          }} />
+        </View>
+      </ImageBackground>
     </Swiper>
   );
 };
@@ -126,21 +190,19 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
-    height: 'auto',
-    width: 'auto',
+    height: '100%',
+    width: '100%',
     resizeMode: 'cover',
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   genderContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000',
   },
   genderImagesContainer: {
     justifyContent: 'space-between',
@@ -151,12 +213,12 @@ const styles = StyleSheet.create({
   genderImageWrapper: {
     alignItems: 'center',
     marginHorizontal: 10,
-    padding:20
+    padding: 20
   },
   genderImage: {
     width: 200,
     height: 200,
-    borderRadius: 75, // Circular image
+    borderRadius: 75,
     borderWidth: 2,
     borderColor: '#4CAF50',
   },
@@ -171,6 +233,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#fff',
     marginBottom: 20,
+    alignSelf: 'center',
+    textAlign: 'center'
   },
   button: {
     backgroundColor: '#4CAF50',
@@ -181,17 +245,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignSelf: 'center',
   },
-  goalButton:{
+  goalButton: {
     backgroundColor: '#000',
-    // padding: 20,
     marginBottom: 50,
-    borderRadius:15,
-    // justifyContent: 'space-around',
-    borderWidth:2,
-    borderColor:'#4CAF50',
-    // width: 200,
-    alignItems: 'center',
-    // alignSelf: 'center',
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: '#4CAF50',
+    alignItems: 'center'
   },
   startButton: {
     backgroundColor: '#4CAF50',
@@ -208,9 +268,6 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
-  },
-  loseWeight:{
-    backgroundImage:'../../assets/images/weight-loss.png'
   },
   startBtn: {
     color: '#fff',
@@ -229,7 +286,7 @@ const styles = StyleSheet.create({
   },
   backButton: {
     position: 'absolute',
-    top: 50,
+    top: 30,
     left: 20,
     backgroundColor: '#000',
     padding: 10,
@@ -239,10 +296,10 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
   },
-  goalImage:{
+  goalImage: {
     width: 200,
     height: 100,
     borderTopLeftRadius: 15,
-    borderTopRightRadius:15,
+    borderTopRightRadius: 15,
   }
 });
